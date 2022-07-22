c999486fa6521b2bdbfc7e4b55b98390a7c3a54a
Tatiana <43042778+tygragamand@users.noreply.github.com>
2022-07-21 22:07:31 -0400

    Add files via upload

---
+++ allocation.c
@@ -0,0 +1,31 @@
+/*
+------------------------------------------------------------------------------------------
+File:    allocation.c
+Purpose: Assignment 4, Question 1
+------------------------------------------------------------------------------------------
+Author:  Alex Lau (GitHub: BobahhBoi) , Tatiana Olenciuc (GitHub: tygragamand)
+Repository: https://github.com/BobahhBoi/CP386_A4
+Version  2022-07-22
+------------------------------------------------------------------------------------------
+*/
+
+//Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+
+
+    //first fit
+
+
+    //best fit
+
+
+    //worst fit
+    
+    return 0;
+}
